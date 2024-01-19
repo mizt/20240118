e082e103d3cb1ada7d2339c4387e96e9fac9e180
mizt <info@mizt.org>
2024-01-19 09:34:43 +0900

    Update

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,8 @@
 cd "$(dirname "$0")"
 cd ./
 
+set -eu
+
 clang++ -O3 -std=c++20 -framework Cocoa -Wc++17-extensions ./failure.mm -o ./failure
 clang++ -O3 -std=c++20 -framework Cocoa -Wc++17-extensions ./success.mm -o ./success
 clang++ -O3 -std=c++20 -framework Cocoa -Wc++17-extensions ./argument.mm -o ./argument
